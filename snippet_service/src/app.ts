2712cd9094df9829d4184be1d6ffb5773eaabc1d
felifernandezz <felifernandezuni@gmail.com>
2025-11-27 14:58:45 -0300

    Snippet Service funcionando

---
+++ snippet_service/src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Snippet Service is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
